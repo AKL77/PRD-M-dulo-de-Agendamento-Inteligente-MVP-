46da2b8ae07f44ad5b70646156dfd6a541404690
Augusto Lunardi <augustolunardi12@gmail.com>
2026-04-23 14:32:16 -0300

    teste definir horario medico mais completo

--- testAgenda.py
+++ testAgenda.py
@@ -4,4 +4,7 @@ class TestAgenda(TestCase):
     def test_definir_horario_medico(self):
         agenda = Agenda()
 
-        
+        agenda.configurar_expediente_medico(inicio = "08:00", fim = "12:00")
+
+        self.assertEqual(agenda.horario_inicio, "08:00")
+        self.assertEqual(agenda.horario_fim, "12:00")
