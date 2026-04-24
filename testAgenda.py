71f4492a2dc79a2665e6da23efe082590469930c
Augusto Lunardi <augustolunardi12@gmail.com>
2026-04-24 13:11:50 -0300

    teste agendamento consulta em horario ja ocupado implementado

--- testAgenda.py
+++ testAgenda.py
@@ -18,7 +18,7 @@ class TestAgenda(TestCase):
 
         horario_consulta = datetime(2026, 4, 24, 9, 0)
 
-        paciente = "03114976085"
+        paciente = "031.149.760-85"
 
         agenda.agendar_consulta(paciente, horario_consulta)
 
@@ -31,11 +31,30 @@ class TestAgenda(TestCase):
 
         horario_consulta = datetime(2026, 4, 24, 7, 0)
 
-        paciente = "03114976085"
+        paciente = "031.149.760-85"
 
         agendamento = agenda.agendar_consulta(paciente, horario_consulta)
 
         self.assertEqual(agendamento, "O horário informado não é contemplado pelo médico")
 
         self.assertNotIn(horario_consulta, agenda.consultas_marcadas)
+
+    def test_agendamento_consulta_horario_ja_agendado(self):
+        agenda = Agenda()
+
+        agenda.configurar_expediente_medico(inicio = time(8, 0), fim = time(12, 0))
+
+        horario_consulta_1 = datetime(2026, 4, 24, 8, 0)
+        paciente_1 = "031.149.760-85"
+        agendamento_1 = agenda.agendar_consulta(paciente_1, horario_consulta_1)
+
+        self.assertEqual(agenda.consultas_marcadas[horario_consulta_1], paciente_1)
+
+        horario_consulta_2 = datetime(2026, 4, 24, 8, 0)
+        paciente_2 = "777.777.777-77"
+        agendamento_2 = agenda.agendar_consulta(paciente_2, horario_consulta_2)
+
+        self.assertEqual(agendamento_2, "O horário informado não já está ocupado por outro agendamento. Escolha outro horário")
+
+
 
